863e95f5600b86b1c4cdd4e41c93360873843c4d
Sam Puttman <daputter2@gmail.com>
2024-06-03 12:03:02 -0500

    Remove all legacy rails code. Refactor application into barebones Sinatra app. Set up Procfile

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'json'
+require_relative 'services/plant_generator'
+
+set :port, 3001 
+
+before do
+  if request.content_type == 'application/json'
+    request.body.rewind
+    @request_payload = JSON.parse(request.body.read)
+  end
+end
+
+post '/generate_plant' do
+  content_type :json
+
+  name = @request_payload['name']
+  description = @request_payload['description']
+  plant_type = @request_payload['plant_type']
+
+  plant = PlantGenerator.create(name: name, description: description, plant_type: plant_type)
+  plant.to_json
+end
